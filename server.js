7aa62b32b599663e7f99278d7a4fd286a259d4dc
Гойгов Саид-Амин У <student128@ithub.local>
2026-09-22 10:20:36 +0300

    lesson_new2

--- server.js
+++ server.js
@@ -13,13 +13,26 @@ app.get('/about' , (req, res) =>{
     res.send("О нас")
 })
 
+app.get('/contacts' , (req, res) =>{
+    res.send("Контакты")
+})
 
+app.get('/product/:id' , (req, res) =>{
+    console.log(req.params.id);
+    
+    res.send(`Продукт id: ${req.params.id}`)
+})
 
+app.get('/number/:id' , (req, res) =>{
+    console.log(req.params.id);
+    
+    res.send(`Ваш номер телефона: ${req.params.id}`)
+})
 
-// const server = http.createServer((req, res)=>{ // создаем сервер
-//     res.end("Hello World") // ответ клиенту когда он зайдет на наш сервер 
-
-// })
+app.post('/addproduct' , (req, res) =>{
+    const { name , age} = req.body;
+    
+})
 
 app.listen(PORT , ()=>{ // тут расписываем на каком порту запускается сервер 
     console.log("Сервер запущен успешно по адресу: 127.0.0.1:3000"); // тут делаем уведомление для себя что сервер запущен
